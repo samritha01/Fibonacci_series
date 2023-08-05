acac2709ad2d7dac52870c272d6f97e2e59a73f7
samritha01 <sammykite2001@gmail.com>
2023-08-05 13:45:44 -0700

    commit

---
+++ Fibonacci.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a0=0,a1=1,nextTerm=0,n;
+    cout<<"Enter n:";
+    cin>>n;
+    cout<<"Fibonacci series:"<<a0<<", "<<a1<<" ,";
+    nextTerm=a0+a1;
+    
+    while(nextTerm<=n)
+    {
+        cout<<nextTerm<<", ";
+        a0=a1;
+        a1=nextTerm;
+        nextTerm=a0+a1;
+        
+    }
+    return 0;
+}
